4c68bea3daab8895add740daeec0c1c730c08d79
Max Shen <gshen@college.harvard.edu>
2014-12-01 00:49:40 -0500

    debugging

--- Ask-Harvard/client/templates/homepage.js
+++ Ask-Harvard/client/templates/homepage.js
@@ -1,12 +1,3 @@
-// allows client to obtain data from the main database
-// add stuff here to control what data the client can obtain
-Meteor.subscribe('Tasks');
-
-// enable username and email for login
-Accounts.ui.config({
-    passwordSignupFields: "USERNAME_AND_EMAIL"
-  });
-
 //functions that do work on the homepage.html
 Template.homepage.helpers({
     tasks: function () {
