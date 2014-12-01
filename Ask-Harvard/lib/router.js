7a2c61476e514a5324af67a5b0bdfcf0d7d752e5
Max Shen <gshen@college.harvard.edu>
2014-12-01 02:15:29 -0500

    removed loading spinner- doesnt work yet

--- Ask-Harvard/lib/router.js
+++ Ask-Harvard/lib/router.js
@@ -1,8 +1,10 @@
 Router.configure({
   // loads layout.html on every single page
   layoutTemplate: 'layout',
+  
   // shows spinning wheel while loading for better user interface
-  loadingTemplate: 'loading',
+  // (DOES NOT WORK YET) loadingTemplate: 'loading',
+
   // allows client to obtain data from the main database
   // add stuff here to control what data the client can obtain
   waitOn: function() { return Meteor.subscribe('Tasks'); }
