383a6e4a90528cc6921e337bd989291a1fe21922
Max Shen <gshen@college.harvard.edu>
2014-12-01 15:02:52 -0500

    email verification

---
+++ Ask-Harvard/server/smtp.js
@@ -0,0 +1,40 @@
+Meteor.startup(function () {
+  smtp = {
+    username: 'gshen@college.harvard.edu',   // eg: server@gentlenode.com
+    password: 'Sxd1996^^',   // eg: 3eeP1gtizk5eziohfervU
+    server:   'smtp.gmail.com',  // eg: mail.gandi.net
+    port: 25
+  }
+
+  process.env.MAIL_URL = 'smtp://' + encodeURIComponent(smtp.username) + ':' + encodeURIComponent(smtp.password) + '@' + encodeURIComponent(smtp.server) + ':' + smtp.port;
+});
+
+Accounts.onCreateUser(function(options, user) {
+  user.profile = {};
+
+  // we wait for Meteor to create the user before sending an email
+  Meteor.setTimeout(function() {
+    Accounts.sendVerificationEmail(user._id);
+  }, 2 * 1000);
+
+  return user;
+});
+
+Meteor.startup(function() {
+  // By default, the email is sent from no-reply@meteor.com. If you wish to receive email from users asking for help with their account, be sure to set this to an email address that you can receive email at.
+  Accounts.emailTemplates.from = 'Gentlenode <no-reply@gentlenode.com>';
+
+  // The public name of your application. Defaults to the DNS name of the application (eg: awesome.meteor.com).
+  Accounts.emailTemplates.siteName = 'Gentlenode Studio';
+
+  // A Function that takes a user object and returns a String for the subject line of the email.
+  Accounts.emailTemplates.verifyEmail.subject = function(user) {
+    return 'Confirm Your Email Address';
+  };
+
+  // A Function that takes a user object and a url, and returns the body text for the email.
+  // Note: if you need to return HTML instead, use Accounts.emailTemplates.verifyEmail.html
+  Accounts.emailTemplates.verifyEmail.text = function(user, url) {
+    return 'click on the following link to verify your email address: ' + url;
+  };
+});
