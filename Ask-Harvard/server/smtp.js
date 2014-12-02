7a1e910fc0c3b10280eeb2ca4d99edf33e32164d
Max Shen <gshen@college.harvard.edu>
2014-12-01 23:07:41 -0500

    debug

--- Ask-Harvard/server/smtp.js
+++ Ask-Harvard/server/smtp.js
@@ -45,9 +45,8 @@ Meteor.startup(function() {
 Accounts.validateLoginAttempt(function(attempt){
   if (attempt.user && attempt.user.emails && !attempt.user.emails[0].verified ) {
     console.log('email not verified');
-
-    return false; // the login is aborted
     Router.go('/mustverify');
+    return false; // the login is aborted
   }
   return true;
 }); 
