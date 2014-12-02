763a39249723eba4cafa473798c2f1223e077cfd
Max Shen <gshen@college.harvard.edu>
2014-12-02 16:38:20 -0500

    deleting your own questions

--- Ask-Harvard/client/templates/task.js
+++ Ask-Harvard/client/templates/task.js
@@ -1,6 +1,13 @@
+// remove task if clicked
 Template.task.events({
   "click .delete": function () {
     Tasks.remove(this._id);
   }
 });
 
+// checks whether post belongs to the current user
+Template.task.helpers({
+  ownPost: function(){
+    return this.owner === Meteor.userId();
+  }
+});
--- Ask-Harvard/lib/collections/tasks.js
+++ Ask-Harvard/lib/collections/tasks.js
@@ -1,2 +1,13 @@
+// Start new mongo database for questions
 Tasks = new Mongo.Collection("tasks");
 
+Tasks.allow({  
+  insert: function(userId, doc) {    
+  // only allow posting if you are logged in    
+    return !! userId;
+  },
+  remove: function(userId, doc) {    
+  // only allow deleting if you are logged in    
+    return !! userId;
+  }
+});
