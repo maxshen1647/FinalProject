b97a5dcd9a6d1e7be99e57ef65e949e1b8777a0e
Max Shen <gshen@college.harvard.edu>
2014-12-03 23:22:03 -0500

    added the division between harvard and regular

--- Ask-Harvard/client/templates/answerpage.js
+++ Ask-Harvard/client/templates/answerpage.js
@@ -2,5 +2,12 @@ Template.answerpage.helpers({
 	// returns all of the comments on the current post
 	comments: function() {    
 		return Comments.find({postId: this._id});  
-	}
+	},
+	// checks whether current user is registered with a college.harvard.edu email
+ 	harvardStudent: function(){
+	  	var email = Meteor.user().emails[0].address;
+	  	var n = email.indexOf("@");
+	  	var extension = email.substring(n);
+    	return extension === "@college.harvard.edu";
+  	}
 });
