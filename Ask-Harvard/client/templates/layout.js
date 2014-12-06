6b768f1e8dbb25b3da112ad370bb3921e07008f1
Max Shen <gshen@college.harvard.edu>
2014-12-06 13:05:53 -0500

    fixed bug

--- Ask-Harvard/client/templates/layout.js
+++ Ask-Harvard/client/templates/layout.js
@@ -14,18 +14,3 @@ Template.layout.helpers({
     return Comments.find().count();
     }
   });
-
-// verification popup if homepage is induced by verification link
-Template.Homepage.created = function() {
-  if (Accounts._verifyEmailToken) {
-    Accounts.verifyEmail(Accounts._verifyEmailToken, function(err) {
-      if (err != null) {
-        if (err.message = 'Verify email link expired [403]') {
-          console.log('Sorry this verification link has expired.')
-        }
-      } else {
-        console.log('Thank you! Your email address has been confirmed.')
-      }
-    });
-  }
-};
