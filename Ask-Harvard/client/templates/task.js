f6bfd6d94505f63137607206c856c69ba7d5ddcb
Max Shen <gshen@college.harvard.edu>
2014-12-06 15:07:15 -0500

    took out redundant code

--- Ask-Harvard/client/templates/task.js
+++ Ask-Harvard/client/templates/task.js
@@ -12,13 +12,6 @@ Template.task.helpers({
   ownPost: function(){
     return this.userId === Meteor.userId();
   },
-  // checks whether current user is registered with a college.harvard.edu email
-  harvardStudent: function(){
-      var email = Meteor.user().emails[0].address;
-      var n = email.indexOf("@");
-      var extension = email.substring(n);
-      return extension === "@college.harvard.edu";
-  },
   // counts the number of answers on the current question
   commentsCount: function() {   
   	return Comments.find({postId: this._id}).count();  
